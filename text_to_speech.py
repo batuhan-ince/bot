955618a3304e1ae512e66472c7c4ebb8ff37b08d
yusufhilmi <177yusuf177@gmail.com>
2019-02-07 20:06:36 +0300

    second

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,12 +1,15 @@
 from espeak import espeak
 from datetime import datetime
 from subprocess import call
+import time
+import os
 def time_now():
     t=datetime.now().strftime("%-d %B %Y %A")
     call(["espeak","-s140","-g10","-ven+f4","The date is %s"%t])
 def hello_user():
     call(["espeak","-s180","-ven+f4","Hello! Text to speech activated."])
 
-def ayar():
-    call(["jack -d alsa"])
-    
+def xterm():
+    os.system("xterm -e \" jackd -d alsa\"&")
+
+
